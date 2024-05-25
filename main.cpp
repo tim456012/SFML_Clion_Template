6528f9da5acc415dccf6af1bc3379dbd6d6567f5
chc_Danny <tim168950@gmail.com>
2024-05-25 10:15:28 +0800

    Add glad library and update CMakeLists

--- main.cpp
+++ main.cpp
@@ -1,63 +1,153 @@
-#include "imgui.h"
-#include "imgui-SFML.h"
+#include <glad/glad.h>
+#include <imgui-SFML.h>
+#include <imgui.h>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
-#include "SFML/Graphics.hpp"
+const char* vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800,600), "Hello, World!");
-    ImGui::SFML::Init(window);
+const char* fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
 
-    bool circleExists = true;
-    float circleRadius = 200.f;
-    int circleSegments = 100;
-    float circleColors[3] = {(float)204/255, (float)77/255, (float)5/255 };
+// clang-format off
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+// clang-format on
 
-    sf::CircleShape shape(circleRadius, 100);
+int
+main()
+{
+    // Create an SFML window with OpenGL context
+    sf::RenderWindow window(
+        sf::VideoMode(1280, 720), "Hello, World", sf::Style::Default, sf::ContextSettings(24));
+    window.setVerticalSyncEnabled(true);
+    window.setActive();
 
+    // Initialize OpenGL using glad AFTER creating the window
+    if (! gladLoadGL())
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
-        sf::Event event{};
-        while (window.pollEvent(event)) {
+    // Initialize ImGui-SFML
+    if (! ImGui::SFML::Init(window))
+    {
+        std::cerr << "Failed to initialize ImGui-SFML!" << std::endl;
+        return 1;
+    }
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    int success;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (! success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int fragementShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragementShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragementShader);
+
+    glGetShaderiv(fragementShader, GL_COMPILE_STATUS, &success);
+    if (! success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(fragementShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragementShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(shaderProgram);
+    glDeleteShader(fragementShader);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (! success)
+    {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+    glEnableVertexAttribArray(0);
+
+    // // Clock for delta time calculation
+    // sf::Clock deltaClock;
+
+    bool isRunning = true;
+    while (isRunning)
+    {
+        // Handle events
+        sf::Event event {};
+        while (window.pollEvent(event))
+        {
             ImGui::SFML::ProcessEvent(event);
-            if(event.type == sf::Event::Closed)
+
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
+                isRunning = false;
+            }
         }
 
-        ImGui::SFML::Update(window, deltaClock.restart());
+        // // Start a new ImGui frame
+        // ImGui::SFML::Update(window, deltaClock.restart());
+        // ImGui::ShowDemoWindow();
 
-        ImGui::Begin("Title");
-        ImGui::Text("Hello World");
-        ImGui::Checkbox("Circle", &circleExists);
-        ImGui::SliderFloat("Radius", &circleRadius, 100.f, 300.f);
-        ImGui::SliderInt("Sides", &circleSegments, 3, 150);
-        ImGui::ColorEdit3("Circle Color", circleColors);
-        ImGui::End();
+        // Clear the window and OpenGL buffers
+        glClearColor(0.404f, 0.576f, 0.643f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        shape.setRadius(circleRadius);
-        shape.setFillColor(sf::Color(
-            (int)(circleColors[0] * 255),
-            (int)(circleColors[1] * 255),
-            (int)(circleColors[2] * 255)
-            ));
+        // // Render ImGui on top of the OpenGL content
+        // ImGui::SFML::Render(window);
 
-        float windowWidth = static_cast<float>(window.getSize().x);
-        float windowHeight = static_cast<float>(window.getSize().y);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        float centerX = (windowWidth - 2 * shape.getRadius()) / 2;
-        float centerY = (windowHeight - 2 * shape.getRadius()) / 2;
-
-        shape.setPosition(centerX, centerY);
-        shape.setPointCount(circleSegments);
-
-        window.clear(sf::Color(18,33,43));
-        if (circleExists) {
-            window.draw(shape);
-        }
-        ImGui::SFML::Render(window);
+        // Display the final frame
         window.display();
     }
 
-    ImGui::SFML::Shutdown();
+    // // Cleanup ImGui-SFML
+    // ImGui::SFML::Shutdown();
+
     return 0;
 }
