9949dc9f287f05307ec14c1c8a0ba73eb2be1b49
chc_Danny <tim168950@gmail.com>
2024-05-25 23:01:48 +0800

    Comment out glad functions implementation

--- main.cpp
+++ main.cpp
@@ -5,29 +5,29 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-const char* vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-
-const char* fragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
-
-// clang-format off
-float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-};
-// clang-format on
+// const char* vertexShaderSource =
+//     "#version 330 core\n"
+//     "layout (location = 0) in vec3 aPos;\n"
+//     "void main()\n"
+//     "{\n"
+//     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//     "}\0";
+//
+// const char* fragmentShaderSource =
+//     "#version 330 core\n"
+//     "out vec4 FragColor;\n"
+//     "void main()\n"
+//     "{\n"
+//     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+//     "}\0";
+//
+// // clang-format off
+// float vertices[] = {
+//     -0.5f, -0.5f, 0.0f,
+//      0.5f, -0.5f, 0.0f,
+//      0.0f,  0.5f, 0.0f
+// };
+// // clang-format on
 
 int
 main()
@@ -38,12 +38,12 @@ main()
     window.setVerticalSyncEnabled(true);
     window.setActive();
 
-    // Initialize OpenGL using glad AFTER creating the window
-    if (! gladLoadGL())
-    {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+    // // Initialize OpenGL using glad AFTER creating the window
+    // if (! gladLoadGL())
+    // {
+    //     std::cerr << "Failed to initialize GLAD" << std::endl;
+    //     return -1;
+    // }
 
     // Initialize ImGui-SFML
     if (! ImGui::SFML::Init(window))
@@ -52,64 +52,64 @@ main()
         return 1;
     }
 
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
+    // unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    // glCompileShader(vertexShader);
 
-    int success;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    // int success;
+    // glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 
-    if (! success)
-    {
-        char infoLog[512];
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
+    // if (! success)
+    // {
+    //     char infoLog[512];
+    //     glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+    //     std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    // }
 
-    unsigned int fragementShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragementShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragementShader);
+    // unsigned int fragementShader = glCreateShader(GL_FRAGMENT_SHADER);
+    // glShaderSource(fragementShader, 1, &fragmentShaderSource, nullptr);
+    // glCompileShader(fragementShader);
+    //
+    // glGetShaderiv(fragementShader, GL_COMPILE_STATUS, &success);
+    // if (! success)
+    // {
+    //     char infoLog[512];
+    //     glGetShaderInfoLog(fragementShader, 512, nullptr, infoLog);
+    //     std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    // }
 
-    glGetShaderiv(fragementShader, GL_COMPILE_STATUS, &success);
-    if (! success)
-    {
-        char infoLog[512];
-        glGetShaderInfoLog(fragementShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
+    // unsigned int shaderProgram = glCreateProgram();
+    // glAttachShader(shaderProgram, vertexShader);
+    // glAttachShader(shaderProgram, fragementShader);
+    // glLinkProgram(shaderProgram);
+    //
+    // glDeleteShader(shaderProgram);
+    // glDeleteShader(fragementShader);
+    //
+    // glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    // if (! success)
+    // {
+    //     char infoLog[512];
+    //     glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+    //     std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+    // }
+    //
+    // unsigned int VBO;
+    // glGenBuffers(1, &VBO);
+    //
+    // unsigned int VAO;
+    // glGenVertexArrays(1, &VAO);
+    //
+    // glBindVertexArray(VAO);
+    //
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    // static_cast<void*>(nullptr)); glEnableVertexAttribArray(0);
 
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragementShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(shaderProgram);
-    glDeleteShader(fragementShader);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (! success)
-    {
-        char infoLog[512];
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
-    }
-
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
-    glEnableVertexAttribArray(0);
-
-    // // Clock for delta time calculation
-    // sf::Clock deltaClock;
+    // Clock for delta time calculation
+    sf::Clock deltaClock;
 
     bool isRunning = true;
     while (isRunning)
@@ -127,27 +127,29 @@ main()
             }
         }
 
-        // // Start a new ImGui frame
-        // ImGui::SFML::Update(window, deltaClock.restart());
-        // ImGui::ShowDemoWindow();
+        // Start a new ImGui frame
+        ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::ShowDemoWindow();
 
-        // Clear the window and OpenGL buffers
-        glClearColor(0.404f, 0.576f, 0.643f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // // Clear the window and OpenGL buffers
+        // glClearColor(0.451f, 0.604f, 0.678f, 1.0f);
+        // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // // Render ImGui on top of the OpenGL content
-        // ImGui::SFML::Render(window);
+        window.clear(sf::Color(45, 60, 68, 255));
 
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // Render ImGui on top of the OpenGL content
+        ImGui::SFML::Render(window);
+
+        // glUseProgram(shaderProgram);
+        // glBindVertexArray(VAO);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Display the final frame
         window.display();
     }
 
-    // // Cleanup ImGui-SFML
-    // ImGui::SFML::Shutdown();
+    // Cleanup ImGui-SFML
+    ImGui::SFML::Shutdown();
 
     return 0;
 }
